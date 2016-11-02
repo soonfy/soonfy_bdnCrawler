ed58b9db19b3da923f02030980091997d5e51c27
soonfy <soonfy@163.com>
2016-11-02 15:28:29 +0800

    no log

--- app/index.js
+++ app/index.js
@@ -4,5 +4,4 @@ const config_js_1 = require('./config.js');
 const crawler_js_1 = require('./controllers/crawler.js');
 mongoose.connect(config_js_1.Config.dbUrl);
 mongoose.Promise = global.Promise;
-mongoose.set('debug', true);
 crawler_js_1.Crawler.start();
