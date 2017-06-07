042d06d6367f3d69965e543d686955aa27ac0039
soonfy <soonfy@163.com>
2017-06-07 17:20:34 +0800

    crawl articles

--- crawl_article.js
+++ crawl_article.js
@@ -7,8 +7,17 @@ import {
 
 import * as fs from 'fs';
 import * as os from 'os';
+import * as path from 'path';
+import * as url from 'url';
+const URL = require('url').URL;
 
+import * as _ from 'lodash';
 import * as moment from 'moment';
+import * as filer from 'filer_sf';
+
+import {
+  Config
+} from './config.js';
 
 const starter = async() => {
   try {
@@ -67,4 +76,224 @@ const starter = async() => {
   }
 }
 
-starter();
+// starter();
+
+let crawl = async(param) => {
+  try {
+    let $ = await Parser.getData(param);
+    let pages = Parser.moreParser($);
+    let results = Parser.dataParser($);
+    // 相同新闻
+    console.log(pages);
+    for (let page of pages) {
+      let param = page;
+      let $ = await Parser.getData(param);
+      results = results.concat(Parser.dataParser($));
+    }
+    console.log(results.length);
+    return results;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const newer = async() => {
+  try {
+    let host = Config.host;
+    let filepath = path.join(__dirname, '../source/demo.xlsx');
+    let uripath = path.join(__dirname, '../source/uri.xlsx');
+    let resultpath;
+    let articlepath;
+    let data = filer.read(filepath);
+    let results = [];
+    // console.log(data);
+    let topics = data['监测关键词'];
+    let reposts = data['转载关键词'];
+
+    let news = data['重大新闻关键词'];
+    news.shift();
+    let new_ls = [];
+    news.map(x => {
+      x.map(y => new_ls.push(y));
+    })
+    console.log(new_ls);
+
+    let uris = filer.read(uripath);
+    uris = uris['官网链接'];
+    uris.shift();
+    let uri_ls = [];
+    uris.map(x => {
+      x[3] ? uri_ls.push(x[3]) : uri_ls.push('');
+    })
+    console.log(uri_ls);
+
+    let head = topics.shift().slice(0, 5);
+    head.push('渠道');
+    head.push('链接');
+    head.push('转载量');
+    console.log(head);
+    results.push(head);
+
+    let index = 0;
+    for (let line of topics) {
+      console.log(line[2]);
+      let keys = line.slice(5);
+      let articles = [];
+      let sum = 0;
+      let hostname = null;
+      if (uri_ls[index] && uri_ls[index].includes('http')) {
+        hostname = new URL(uri_ls[index]).hostname;
+        let names = hostname.split('.');
+        hostname = names.slice(-2, -1).pop() + '.';
+      }
+      console.log(++index);
+      console.log('hostname', hostname);
+      let date = '2017-05-01';
+      while (date.includes('2017-05')) {
+        console.log(date);
+
+        // 
+        // key
+        // resultpath = path.join(__dirname, '../source/渠道.xlsx');
+        // articlepath = path.join(__dirname, '../source/渠道内容.xlsx');
+        // for (let key of keys) {
+        //   let q1 = key;
+        //   // console.log(q1);
+        //   let keyer = {
+        //     key: {
+        //       q1: q1
+        //     },
+        //     date: {
+        //       begin_date: date,
+        //       end_date: date
+        //     }
+        //   }
+        //   let params = ParamsParser.parse(keyer);
+        //   let urls = [];
+        //   for (let key in params) {
+        //     urls.push([key, params[key]].join('='))
+        //   }
+        //   let param = urls.join('&');
+        //   param = ['/ns', param].join('?');
+        //   let uri = host + param;
+        //   let _articles = await crawl(param);
+
+        //   // 下一页
+        //   let $ = await Parser.getData(param);
+        //   let next = Parser.pageParser($);
+        //   while (next) {
+        //     // console.log(next);
+        //     let param = next;
+        //     _articles = _.concat(_articles, await crawl(param));
+        //     let $ = await Parser.getData(param);
+        //     next = Parser.pageParser($);
+        //   }
+        //   // console.log(q1, 'parse over.');
+        //   if (q1.includes('央视网')) {
+        //     let len = _articles.length;
+        //     _articles = _articles.slice(0, Math.round(len / 15));
+        //   }
+        //   // let icont = cont.slice(0, 3);
+        //   // icont.push(date);
+        //   // icont.push(date);
+        //   // icont.push(q1);
+        //   // icont.push(uri);
+        //   // icont.push(_articles.length);
+        //   // results.push(icont);
+        //   articles = _.concat(articles, _articles);
+        //   _articles = null;
+        // }
+
+        // 
+        // key + news
+        resultpath = path.join(__dirname, '../source/新闻.xlsx');
+        articlepath = path.join(__dirname, '../source/新闻内容.xlsx');
+        for (let key of keys) {
+          for (let _news of new_ls) {
+            let news_l = [key];
+            news_l = news_l.concat(_news.split('+'));
+            news_l = news_l.filter(x => x && x.trim());
+            let q1 = news_l.join(' ');
+            // console.log(q1);
+            let keyer = {
+              key: {
+                q1: q1
+              },
+              date: {
+                begin_date: date,
+                end_date: date
+              }
+            }
+            let params = ParamsParser.parse(keyer);
+            let urls = [];
+            for (let key in params) {
+              urls.push([key, params[key]].join('='))
+            }
+            let param = urls.join('&');
+            param = ['/ns', param].join('?');
+            let uri = host + param;
+            let _articles = await crawl(param);
+
+            // 下一页
+            let $ = await Parser.getData(param);
+            let next = Parser.pageParser($);
+            while (next) {
+              // console.log(next);
+              let param = next;
+              _articles = _.concat(_articles, await crawl(param));
+              let $ = await Parser.getData(param);
+              next = Parser.pageParser($);
+            }
+            // console.log(q1, 'parse over.');
+            if (q1.includes('央视网')) {
+              let len = _articles.length;
+              _articles = _articles.slice(0, Math.round(len / 15));
+            }
+            // let icont = cont.slice(0, 3);
+            // icont.push(date);
+            // icont.push(date);
+            // icont.push(q1);
+            // icont.push(uri);
+            // icont.push(_articles.length);
+            // results.push(icont);
+            articles = _.concat(articles, _articles);
+            _articles = null;
+          }
+        }
+
+        date = moment(new Date(date) - 0 + 1000 * 60 * 60 * 24).format('YYYY-MM-DD');
+      }
+
+      console.log(articles.length);
+      if (hostname) {
+        articles = articles.filter(x => {
+          return !x.url.includes(hostname);
+        })
+        console.log(articles.length);
+      }
+      articles = _.uniqBy(articles, 'url');
+      // filer.write(articlepath, articles);
+      sum = articles.length;
+      console.log(sum);
+
+
+      let cont = line.slice(0, 5);
+      cont.push('新闻总量');
+      cont.push('加和');
+      cont.push(sum);
+      results.push(cont);
+      articles = null;
+      cont = null;
+      sum = null;
+      hostname = null;
+      date = null;
+    }
+    console.log(results);
+    filer.write(resultpath, results);
+    console.log(`all over...`);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+newer();
--- crawl_count.js
+++ crawl_count.js
@@ -34,8 +34,18 @@ const starter = async() => {
     })
     // r_words = r_words.slice(0, 1);
     console.log(r_words);
+
     let news = data['重大新闻关键词'];
-    let head = topics.shift().slice(0, 7);
+    news.shift();
+    let new_ls = [];
+    news.map(x => {
+      x.map(y => new_ls.push(y));
+    })
+    console.log(new_ls);
+    let head = topics.shift().slice(0, 5);
+    head.push('渠道');
+    head.push('链接');
+    head.push('转载量');
     console.log(head);
     results.push(head);
     for (let line of topics) {
@@ -43,55 +53,162 @@ const starter = async() => {
       let cont = line.slice(0, 5);
       let keys = line.slice(5);
       let sum = 0;
-      for (let key of keys) {
-        let q3 = '';
-        r_words.map(y => {
-          y = y.replace('XX', key);
-          q3 += y;
-          q3 += '+'
-        })
-        q3 = q3.slice(0, -1);
-        let keyer = {
-          key: {
-            q3: q3
-          },
-          date: {
-            begin_date: line[3].replace(/\./g, '-'),
-            end_date: line[4].replace(/\./g, '-')
+
+      // repost + key
+      // for (let key of keys) {
+      //   for (let repost of r_words) {
+      //     let q1 = repost.replace('XX', key);
+      //     console.log(q1);
+      //     let keyer = {
+      //       key: {
+      //         q1: q1
+      //       },
+      //       date: {
+      //         begin_date: line[3].replace(/\./g, '-'),
+      //         end_date: line[4].replace(/\./g, '-')
+      //       }
+      //     }
+      //     let params = ParamsParser.parse(keyer);
+      //     let urls = [];
+      //     for (let key in params) {
+      //       urls.push([key, params[key]].join('='))
+      //     }
+      //     let param = urls.join('&');
+      //     param = ['/ns', param].join('?');
+      //     let uri = host + param;
+      //     console.log(uri);
+      //     let options = {
+      //       uri,
+      //       timeout: Config.timeout,
+      //       headers: Config.headers,
+      //       transform: function (body) {
+      //         return cheerio.load(body);
+      //       }
+      //     }
+      //     let $ = await rp(options);
+      //     let data = $('.nums').text();
+      //     console.log(data);
+      //     let num = data.replace(/\D/g, '') - 0;
+      //     console.log(num);
+      //     let icont = cont.slice(0);
+      //     icont.push(q1);
+      //     icont.push(uri);
+      //     icont.push(num);
+      //     results.push(icont);
+      //     sum += num;
+      //   }
+      // }
+
+      // http://news.baidu.com/ns?from=news&cl=2&bt=1493568000&y0=2017&m0=5&d0=1&y1=2017&m1=5&d1=31&et=1496246399&q1=&submit=%B0%D9%B6%C8%D2%BB%CF%C2&q3=%C0%B4%D4%B4%D6%D0%D1%EB%B5%E7%CA%D3%CC%A8%D7%DB%BA%CF%C6%B5%B5%C0+%C0%B4%D4%B4CCTV1+%C0%B4%D4%B4CCTV%D7%DB%BA%CF%C6%B5%B5%C0+%C0%B4%D4%B4%D1%EB%CA%D3%CD%F8&q4=&mt=0&lm=&s=2&begin_date=2017-5-1&end_date=2017-5-31&tn=newsdy&ct1=1&ct=1&rn=20&q6=
+      // http://news.baidu.com/ns?from=news&cl=2&bt=1493568000&y0=2017&m0=5&d0=1&y1=2017&m1=5&d1=31&et=1496246399&q1=&submit=%B0%D9%B6%C8%D2%BB%CF%C2&q3=%E6%9D%A5%E6%BA%90%E4%B8%AD%E5%A4%AE%E7%94%B5%E8%A7%86%E5%8F%B0%E7%BB%BC%E5%90%88%E9%A2%91%E9%81%93%2B%E6%9D%A5%E6%BA%90CCTV1%2B%E6%9D%A5%E6%BA%90CCTV%E7%BB%BC%E5%90%88%E9%A2%91%E9%81%93%2B%E6%9D%A5%E6%BA%90%E5%A4%AE%E8%A7%86%E7%BD%91&q4=&mt=0&lm=&s=2&begin_date=2017-05-01&end_date=2017-05-31&tn=newsdy&ct=1&ct1=1&rn=50&q6
+
+      // repost + keys
+      // for (let repost of r_words) {
+      //   let q3 = '';
+      //   keys.map(y => {
+      //     let temp = repost.replace('XX', y);
+      //     q3 += temp;
+      //     q3 += ' '
+      //   })
+      //   q3 = q3.slice(0, -1);
+      //   console.log(q3);
+      //   let keyer = {
+      //     key: {
+      //       q3: q3
+      //     },
+      //     date: {
+      //       begin_date: line[3].replace(/\./g, '-'),
+      //       end_date: line[4].replace(/\./g, '-')
+      //     }
+      //   }
+      //   let params = ParamsParser.parse(keyer);
+      //   let urls = [];
+      //   for (let key in params) {
+      //     urls.push([key, params[key]].join('='))
+      //   }
+      //   let param = urls.join('&');
+      //   param = ['/ns', param].join('?');
+      //   let uri = host + param;
+      //   console.log(uri);
+      //   let options = {
+      //     uri,
+      //     timeout: Config.timeout,
+      //     headers: Config.headers,
+      //     transform: function (body) {
+      //       return cheerio.load(body);
+      //     }
+      //   }
+      //   let $ = await rp(options);
+      //   let data = $('.nums').text();
+      //   console.log(data);
+      //   let num = data.replace(/\D/g, '') - 0;
+      //   console.log(num);
+      //   let icont = cont.slice(0);
+      //   icont.push(q3);
+      //   icont.push(uri);
+      //   icont.push(num);
+      //   results.push(icont);
+      //   sum += num;
+      // }
+
+      // repost + key + news
+      for (let _news of new_ls) {
+        for (let repost of r_words) {
+          for (let key of keys) {
+            let news_l = [repost.replace('XX', key)];
+            news_l = news_l.concat(_news.split('+'));
+            news_l = news_l.filter(x => x && x.trim());
+            let q1 = news_l.join(' ');
+            console.log(q1);
+            let keyer = {
+              key: {
+                q1: q1
+              },
+              date: {
+                begin_date: line[3].replace(/\./g, '-'),
+                end_date: line[4].replace(/\./g, '-')
+              }
+            }
+            let params = ParamsParser.parse(keyer);
+            let urls = [];
+            for (let key in params) {
+              urls.push([key, params[key]].join('='))
+            }
+            let param = urls.join('&');
+            param = ['/ns', param].join('?');
+            let uri = host + param;
+            console.log(uri);
+            let options = {
+              uri,
+              timeout: Config.timeout,
+              headers: Config.headers,
+              transform: function (body) {
+                return cheerio.load(body);
+              }
+            }
+            let $ = await rp(options);
+            let data = $('.nums').text();
+            console.log(data);
+            let num = data.replace(/\D/g, '') - 0;
+            console.log(num);
+            let icont = cont.slice(0);
+            icont.push(q1);
+            icont.push(uri);
+            icont.push(num);
+            results.push(icont);
+            sum += num;
           }
         }
-        let params = ParamsParser.parse(keyer);
-        let urls = [];
-        for (let key in params) {
-          urls.push([key, params[key]].join('='))
-        }
-        let param = urls.join('&');
-        param = ['/ns', param].join('?');
-        let uri = host + param;
-        console.log(uri);
-        let options = {
-          uri,
-          timeout: Config.timeout,
-          headers: Config.headers,
-          transform: function (body) {
-            return cheerio.load(body);
-          }
-        }
-        let $ = await rp(options);
-        let data = $('.nums').text();
-        console.log(data);
-        let num = data.replace(/\D/g, '') - 0;
-        console.log(num);
-        let icont = cont.slice(0);
-        icont.push(q3);
-        icont.push(num);
-        results.push(icont);
-        sum += num;
       }
+
       console.log('新闻总量', sum);
       cont.push('新闻总量');
+      cont.push('加和');
       cont.push(sum);
       results.push(cont);
+
+      filer.write(resultpath, results);
+      process.exit();
     }
     console.log(results);
     filer.write(resultpath, results);
